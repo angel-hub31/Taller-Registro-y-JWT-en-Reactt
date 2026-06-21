7c8a78692f0c71c23a4aaa24b999fed19095880b
angel-hub31 <moralesmales1@hotmail.com>
2026-06-20 23:22:09 -0500

    Paso 2: Configurar la Ruta Pública en src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import './App.css'
 import ProtectedRoute from './components/ProtectedRoute'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext'; // Asegúrate de importar el Provider
-import Login from './pages/Login'; 
+import Login from './pages/Login';
 import Perfil from './pages/Perfil';
+import Registrar from './pages/Registrar';
 function App() {
   return (
     <AuthProvider>
@@ -11,12 +12,13 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
 
-          {/* Rutas Protegidas: el componente ProtectedRoute envuelve a las rutas privadas */}
+          <Route path="/registrar" element={<Registrar />} />
+
           <Route element={<ProtectedRoute />}>
             <Route path="/perfil" element={<Perfil />} />
           </Route>
 
-          <Route path="*" element={<Navigate to="/login" replace/>}/>
+          <Route path="*" element={<Navigate to="/login" replace />} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
